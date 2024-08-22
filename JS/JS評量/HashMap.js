b606c40c5a9a0b6e9cc3f6d82782ed0a82c2ddb2
Rita_00550345 <rita20920@gmail.com>
2024-08-22 16:27:24 +0800

    JS practice V2 & CR list

--- JS/JS評量/HashMap.js
+++ JS/JS評量/HashMap.js
@@ -5,16 +5,20 @@ var HashMap = function () {
     put: function (k, v) {
       obj[k] = v;
     },
-    keys: function (k) {
+    keys: function () {
       return Object.keys(obj);
     },
     contains: function (key) {
-      for (let k in obj) {
-        if (k === key) {
-          return true;
-        }
-      }
-      return false;
+      return key in obj ;
+
+      /* 原先的方式
+       for (let k in obj) {
+         if (k === key) {
+           return true;
+         }
+       }
+     return false;*/
+
     },
     get: function (k) {
       return obj[k];
